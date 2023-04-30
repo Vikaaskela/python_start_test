25770269098bdadd7352419711109da4e098d398
Vikaaskela <25031952sv@gmail.com>
2023-04-30 18:19:06 +0300

    Update task5-8.py
    vydalyla zaive

--- Lessons8/task5-8.py
+++ Lessons8/task5-8.py
@@ -2,4 +2,3 @@
 #Напишіть функцію, яка переводить число, що означає кількість доларів і центів, в прописний формат. Наприклад:
 #> 123,34
 #> one hundred twenty three dollars thirty four cents
-import num2words
