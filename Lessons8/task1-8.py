2d172cefcef5b23da3cbbaf4ca2046ddc075ad55
Vikaaskela <25031952sv@gmail.com>
2023-04-30 19:06:41 +0300

    Update task1-8.py
    add doc-string

--- Lessons8/task1-8.py
+++ Lessons8/task1-8.py
@@ -2,7 +2,15 @@
 #Напишіть функцію, яка реалізує лінійний пошук елемента у списку цілих чисел. 
 #Якщо такий елемент у списку є, то поверніть індекс, якщо ні, то поверніть число «-1».
 def func (seq, x):
+    """ 
+    Searching for an element in a list of integers
+    Args:
+        seq (list): list of integers
+        x (number): number of compare
+
+    Returns:
+        index: if not, then return the number "-1"
+    """
     return f'{seq.index(x) if x in seq else "-1"}'
- 
 print(func([1,2,3,4,5], 5))
 
